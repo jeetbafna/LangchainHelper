960535cad35aed35fa837667094ea7627f2da890
jeetbafna <25198983+jeetbafna@users.noreply.github.com>
2024-04-07 15:09:18 -0500

    Creating embeddings and pushing to Vector DB

--- ingestion.py
+++ ingestion.py
@@ -1,20 +1,37 @@
 import os
 from langchain_community.document_loaders import ReadTheDocsLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain_community.vectorstores import Pinecone as PineconeLangChain
+from pinecone import Pinecone
 
+pc = Pinecone(
+    api_key = os.environ.get("PINECONE_API_KEY")
+)
 
 def ingest_docs() -> None:
-    loader = ReadTheDocsLoader(path="../api.python.lanchain.com/en/latest")
+    loader = ReadTheDocsLoader(path="../langchain-docs/api.python.langchain.com/en/latest")
     raw_documents = loader.load()
 
     print(f"loaded {len(raw_documents)} documents")
 
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=400, chunk_overlap=50, separators=["\n\n", "\n", " ", ""]
+        chunk_size=1000, chunk_overlap=100, separators=["\n\n", "\n", " ", ""]
     )
 
     documents = text_splitter.split_documents(documents=raw_documents)
 
+    print(f"splitted into {len(documents)} chunks")
 
-if __name__ == "__main":
+    for doc in documents:
+        old_path = doc.metadata["source"]
+        new_url = old_path.replace("../langchain-docs", "https:/")
+        doc.metadata.update({"source": new_url})
+
+    print(f"Going to insert {len(documents)} to Pinecone")
+
+    embeddings = OpenAIEmbeddings()
+    PineconeLangChain.from_documents(documents=documents, embeddings=embeddings, index_name="langchain-doc-index")
+
+if __name__ == "__main__":
     ingest_docs()
