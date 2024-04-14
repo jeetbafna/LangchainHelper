13f2ffdb9e2133f039e1fba0b3bded698846c166
jeetbafna <25198983+jeetbafna@users.noreply.github.com>
2024-04-14 17:40:07 -0500

    Added frontend and chat history

--- backend/core.py
+++ backend/core.py
@@ -10,12 +10,21 @@ from consts import INDEX_NAME
 
 pc = Pinecone(api_key=os.environ.get("PINECONE_API_KEY"))
 
-def run_llm(query:str)->Any:
+
+def run_llm(query: str) -> Any:
     embeddings = OpenAIEmbeddings()
-    docsearch = PineconeLangChain.from_existing_index(index_name=INDEX_NAME, embedding=embeddings)
+    docsearch = PineconeLangChain.from_existing_index(
+        index_name=INDEX_NAME, embedding=embeddings
+    )
     chat = ChatOpenAI(verbose=True, temperature=0)
-    qa = RetrievalQA.from_chain_type(llm=chat, chain_type="stuff", retriever=docsearch.as_retriever(), return_source_documents=True)
+    qa = RetrievalQA.from_chain_type(
+        llm=chat,
+        chain_type="stuff",
+        retriever=docsearch.as_retriever(),
+        return_source_documents=True,
+    )
     return qa({"query": query})
+
 
 if __name__ == "__main__":
     print(run_llm(query="What is LangChain?"))
--- main.py
+++ main.py
@@ -9,6 +9,12 @@ st.header("Jeet Bafna's - Documentation Helper Bot")
 
 prompt = st.text_input("Prompt", placeholder="Enter your prompt here..")
 
+if "user_prompt_history" not in st.session_state:
+    st.session_state["user_prompt_history"] = []
+
+if "chat_answers_history" not in st.session_state:
+    st.session_state["chat_answers_history"] = []
+
 def create_sources_string(source_urls: Set[str]) -> str:
     if not source_urls:
         return ""
@@ -23,6 +29,18 @@ def create_sources_string(source_urls: Set[str]) -> str:
 if prompt:
     with st.spinner("Generating Response.."):
         generated_response = run_llm(query=prompt)
-        sources = set([doc.metadata["source"] for doc in generated_response["source_documents"]])
+        sources = set(
+            [doc.metadata["source"] for doc in generated_response["source_documents"]]
+        )
 
-        formatted_response = (f"{generated_response['result']} \n\n {create_sources_string(sources)}")
+        formatted_response = (
+            f"{generated_response['result']} \n\n {create_sources_string(sources)}"
+        )
+
+        st.session_state["user_prompt_history"].append(prompt)
+        st.session_state["chat_answers_history"].append(formatted_response)
+
+if st.session_state["chat_answers_history"]:
+    for generated_response, user_query in zip( st.session_state["chat_answers_history"],st.session_state["user_prompt_history"]):
+        message(user_query, is_user=True)
+        message(generated_response)
